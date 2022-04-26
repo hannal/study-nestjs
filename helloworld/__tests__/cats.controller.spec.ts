8182e2bc818398415389f777b0619e0f6116e41a
hannal <kay@hannal.net>
2022-04-27 00:13:10 +0900

    REFACTOR prettier

--- helloworld/__tests__/cats.controller.spec.ts
+++ helloworld/__tests__/cats.controller.spec.ts
@@ -1,30 +1,30 @@
-import { CatsController } from "../src/modules/cats/cats.controller";
-import { ICreateCatDto } from "../src/modules/cats/create-cat.dto";
+import { CatsController } from '../src/modules/cats/cats.controller';
+import { ICreateCatDto } from '../src/modules/cats/create-cat.dto';
 
 describe('CatsController', () => {
-  let catsController: CatsController
+  let catsController: CatsController;
 
   beforeEach(() => {
     catsController = new CatsController();
   });
 
   it('findAll()', async () => {
-    const expected = ['hello']
-    const result = await catsController.findAll()
-    expect(Array.isArray(result)).toBeTruthy()
-    expect(new Set(result)).toEqual(new Set(expected))
-  })
+    const expected = ['hello'];
+    const result = await catsController.findAll();
+    expect(Array.isArray(result)).toBeTruthy();
+    expect(new Set(result)).toEqual(new Set(expected));
+  });
 
   it('create()', async () => {
     const payload = {
       name: 'doggy',
-    }
-    const expected = `aka ${payload.name}`
-    expect(await catsController.create(payload)).toMatch(expected)
-  })
+    };
+    const expected = `aka ${payload.name}`;
+    expect(await catsController.create(payload)).toMatch(expected);
+  });
 
   it('findOne()', async () => {
-    const expected = '#1 cat'
-    expect(await catsController.findOne('1')).toBe(expected)
-  })
+    const expected = '#1 cat';
+    expect(await catsController.findOne('1')).toBe(expected);
+  });
 });
--- helloworld/src/modules/cats/cats.controller.ts
+++ helloworld/src/modules/cats/cats.controller.ts
@@ -1,23 +1,31 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { ICreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    async findAll(): Promise<string[]> {
-        return ['hello']
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(): Promise<string[]> {
+    return ['hello'];
+  }
 
-    @Get(':id')
-    @HttpCode(HttpStatus.OK)
-    findOne(@Param('id') id: string): string {
-        return `#${id} cat`
-    }
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  findOne(@Param('id') id: string): string {
+    return `#${id} cat`;
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() data: ICreateCatDto): string {
-      return `create a new cat aka ${data.name}`
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() data: ICreateCatDto): string {
+    return `create a new cat aka ${data.name}`;
+  }
 }
